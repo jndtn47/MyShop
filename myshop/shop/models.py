4968401b9b6360009f89ddfd4454e0438562e906
jndtn47 <jndrn17@yandex.ru>
2022-03-19 09:04:16 +0300

    added cart

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
